999e0b869d87608ca43c45bff1acbd185d787377
arata-t <76896195+arata-t@users.noreply.github.com>
2023-12-29 22:33:12 +0900

    details

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,55 +1,7 @@
-import { View, Text, StyleSheet } from "react-native"
-
-import Header from "../components/Header"
-import MemoListItem from "../components/MemoListItem"
+import { Redirect } from 'expo-router'
 
 const Index = (): JSX.Element => {
-  return (
-    <View style={styles.container}>
-
-      <Header />
-
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>+</Text>
-      </View>
-    </View >
-  )
+  return <Redirect href='./memo/detail' />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff'
-  },
-
-  circleButton: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    backgroundColor: '#467FD3',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    shadowColor: '#000000',
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    shadowOffset: { width: 0, height: 8 },
-    elevation: 8
-  },
-  circleButtonLabel: {
-    color: '#ffffff',
-    fontSize: 40,
-    lineHeight: 48
-  }
-
-
-})
 
 export default Index
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   headerTitle: {
     marginBottom: 8,
     fontSize: 22,
-    lineWeight: 32,
+    lineHeight: 32,
     fontWeight: 'bold',
     color: '#ffffff'
   },
