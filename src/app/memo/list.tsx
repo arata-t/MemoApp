4e9bda7f739ff4e830f279454d08f49281e4ebf5
arata-t <76896195+arata-t@users.noreply.github.com>
2024-01-02 11:43:49 +0900

    get_data

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -6,6 +6,8 @@ import Icon from '../../components/Icon'
 import { router, useNavigation } from 'expo-router'
 import { useEffect } from 'react'
 import LogOutButton from '../../components/LogOutButton'
+import { auth, db } from '../../config'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 
 const handlePress = (): void => {
   router.push('/memo/create')
@@ -18,6 +20,17 @@ const List = (): JSX.Element => {
       headerRight: () => { return <LogOutButton /> }
     })
   }, [])
+  useEffect(() => {
+    if (auth.currentUser === null) { return }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    const q = query(ref, orderBy('updatedAt', 'desc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log('memo', doc.data())
+      })
+    })
+    return unsubscribe
+  })
 
   return (
     <View style={styles.container}>
